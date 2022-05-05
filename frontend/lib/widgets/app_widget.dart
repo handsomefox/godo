bb5d4edb4e09bca6d95fc43fb1227182048e2cf8
suicedek <handsomefox.hf@gmail.com>
2022-05-05 17:21:52 +0300

    Use device theme

--- frontend/lib/widgets/app_widget.dart
+++ frontend/lib/widgets/app_widget.dart
@@ -37,7 +37,7 @@ class _AppWidgetState extends State<AppWidget> {
         ],
         supportedLocales: appSupportedLocales,
         debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.light,
+        themeMode: ThemeMode.system,
         theme: getLightTheme(context),
         darkTheme: getDarkTheme(context),
         title: AppLocalizations.of(context)?.appName == null
